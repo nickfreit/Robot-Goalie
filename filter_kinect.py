cce973d370b112bb64d58b0677a387b8c1dd19d1
Nick Freiter <nickfreit@gmail.com>
2018-12-08 13:50:58 -0600

    changed formula for calc real position, added prediction of final position

--- filter_kinect.py
+++ filter_kinect.py
@@ -1,14 +1,19 @@
 # command to start up kinect camera
 # roslaunch freenect_launch freenect.launch rgb_frame_id:=camera_rgb_optical_frame depth_frame_id:=camera_depth_optical_frame
 
+import collections
+
 import roslib
 #roslib.load_manifest('my_package')
 import sys
 import rospy
 import cv2
+import numpy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
+
 
 class image_converter:
 
@@ -18,6 +23,12 @@ class image_converter:
     self.image_sub = rospy.Subscriber("/camera/depth/image_raw",Image,self.callback)
     self.bgs = cv2.createBackgroundSubtractorMOG2()
     self.bgs.setHistory(150)
+    self.w = 640
+    self.h = 480
+    self.minDistance = -10
+    self.scaleFactor = .0021
+    self.centers = collections.deque(maxlen=30)
+    self.output_file = open("output.txt", "w")
     #self.out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (640, 480))
 
   def callback(self,data):
@@ -27,16 +38,31 @@ class image_converter:
       print(e)
 
     filter_frame, x, y = self.filterFrame(cv_image)
-    #if (x >= 0):
-      #print(str(x) + " " + str(y) + " " + str(cv_image[y][x]))
+    
+    if (x >= 0):
+      real_z = cv_image[y][x]
+      self.centers.append((x, y, real_z))
+      (real_x, real_y) = self.pixelToReal(x, y, real_z)
+      self.output_file.write(str(real_x) + " " + str(real_y) + " " + str(real_z) + '\n')
+    x_plane, y_plane = self.predictPosition()
+    
+    print(x_plane, y_plane)
+    cv2.circle(filter_frame, (int(x_plane), int(y_plane)), 5, 255, 3)
+    
+    
+    
+      #print(str(real_x) + " " + str(real_y) + " " + str(real_z))
+    #for center in self.centers:
+      #cv2.circle(filter_frame, (center[0], center[1]), 5, 255, 3);
     cv2.imshow("Image window", filter_frame)
     #filter_frame = cv2.cvtColor(filter_frame, cv2.COLOR_GRAY2BGR)
     #self.out.write(filter_frame)
     cv2.waitKey(3)
 
   def filterFrame(self, frame):
+    ret, frame = cv2.threshold(frame, 3000, 0, cv2.THRESH_TOZERO_INV)
+    ret, frame = cv2.threshold(frame, 1000, 0, cv2.THRESH_TOZERO)
     frame_mask = self.bgs.apply(frame)
-
     ret, frame_mask = cv2.threshold(frame_mask, 50, 255, cv2.THRESH_BINARY)
     frame_mask = cv2.erode(frame_mask, None, iterations=2)
     frame_mask = cv2.dilate(frame_mask, None, iterations=1)
@@ -49,6 +75,25 @@ class image_converter:
       (x, y) = center
       cv2.circle(frame_mask, (x, y), 5, 0, 3);
     return frame_mask, int(x), int(y)
+
+  def predictPosition(self):
+    x = [i[0] for i in self.centers]
+    y = [i[1] for i in self.centers]
+    z = [i[2] for i in self.centers]
+    p_xz = numpy.polyfit(z, x, 2)
+    p_yz = numpy.polyfit(z, y, 2)
+
+    x_plane = numpy.polyval(p_xz, 1000)
+    y_plane = numpy.polyval(p_yz, 1000)
+    return x_plane, y_plane
+
+  def pixelToReal(self, x, y, real_z):
+    real_x = (x - self.w/2) * (real_z + self.minDistance) * self.scaleFactor
+    real_y = (y - self.h/2) * (real_z + self.minDistance) * self.scaleFactor
+    return real_x, real_y
+
+  def calcCurrentVelocity(self):
+    print center
 
 def main(args):
   ic = image_converter()
